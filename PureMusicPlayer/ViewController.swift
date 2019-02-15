9175214f61498c4d3632bd74c7ad8dad17aa48a8
Yuu Kobayashi <yu.kobayashi@std.code.ac.jp>
2019-02-15 15:37:46 +0900

    Initial Commit

---
+++ PureMusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PureMusicPlayer
+//
+//  Created by Yu Kobayashi on 2019/02/15.
+//  Copyright © 2019 Yu Kobayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
